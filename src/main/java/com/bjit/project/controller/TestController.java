6ece19fdd2e5a162d9d4efa42f6c5787372d41db
md.touhid <md.touhid@bjitgroup.com>
2018-02-20 14:28:38 +0600

    init

---
+++ src/main/java/com/bjit/project/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.bjit.project.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class TestController {
+	
+	@GetMapping(value = "/")
+	public String test() {
+		return "index";
+	}
+}
